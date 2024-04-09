9156515c00a9d3a62280c520b5fabae2ba595994
agung widhiatmojo <agung.widhiatmojo@gmail.com>
2024-04-09 18:38:08 +0700

    add action.sh

---
+++ action.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <param1> <param2> <param3> <param3>"
+    exit 1
+fi
+
+# Extract parameters
+param1=$1
+param2=$2
+param3=$3
+param3=$3
+
+# Execute Python script with parameters
+python action.py "$param1" "$param2" "$param3" "$param3"
